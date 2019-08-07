833b4ea0c3adde2a099446594dc4748d5767b530
unknown <anabella005@yahoo.com.ar>
2019-08-07 09:35:55 -0300

    Primeras tres funciones recibidas

---
+++ Tareas primera clase.R
@@ -0,0 +1,17 @@
+#Funcion de Lucia Bernad:
+
+#Celsius a Fahrenheit
+
+#(0 °C × 9/5) + 32 = 32 °F
+
+a <- c(0,1,10,20,100)
+
+grados <- function(Far)
+  
+{Far= (a*(9/5) + 32)
+
+return(paste(Far,"F"))
+
+}
+
+grados(a)
